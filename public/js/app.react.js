6630091e0a44cafb1a528a2471d89e85e91c1a93
Tal Ater <tal@talater.com>
2016-09-08 13:45:36 +0300

    Added basic non-functioning REPL

--- public/js/app.react.js
+++ public/js/app.react.js
@@ -1,12 +1,47 @@
+var focusOnInput = () => {
+  document.getElementById('repl-text-input').focus();
+};
+
+var REPLoutput = React.createClass({
+  render: () => {
+    return (
+      <div className="repl-output">
+        NAME
+        ls - list directory contents
+        SYNOPSIS
+        ls [OPTION]... [FILE]...
+        DESCRIPTION
+        List  information  about the FILEs (the current directory by default).  Sort entries alpha-
+        betically if none of -cftuvSUX nor --sort is specified.
+        Mandatory arguments to long options are mandatory for short options too.
+      </div>
+    )
+  }
+});
+
+var REPLinput = React.createClass({
+  render: () => {
+    return (
+      <div className="repl-input">
+        <span className="whoami">[tal@ater ~] $</span>
+        <input onBlur={focusOnInput} id="repl-text-input"></input>
+      </div>
+    )
+  }
+});
+
 var REPL = React.createClass({
-  render: function() {
+  componentDidMount: focusOnInput,
+  render: () => {
     return (
       <div className="repl-container">
-        Hello, world!
+        <REPLoutput />
+        <REPLinput />
       </div>
     );
   }
 });
+
 ReactDOM.render(
   <REPL />,
   document.getElementById('repl')
