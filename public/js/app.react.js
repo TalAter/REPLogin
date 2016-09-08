f4426b80a9964a01b06fab9caf471a7bbe81af6d
Tal Ater <tal@talater.com>
2016-09-08 15:40:34 +0300

    Consolidated everything into one React component due to lack of time for this exercise :(

--- public/js/app.react.js
+++ public/js/app.react.js
@@ -7,82 +7,59 @@ var focusOnInput = () => {
   document.getElementById('repl-text-input').focus();
 };
 
-/**************/
-/* REPL logic */
-/**************/
-
-// Reads a command that was just submitted and passes it to EVALUATE
-var REPL_READ = (event) => {
-  event.preventDefault();
-  var input = document.getElementById('repl-text-input').value;
-  REPL_EVALUATE(input);
-};
-
-// Evaluate a command and passes it to PRINT
-var REPL_EVALUATE = (input) => {
-  var output = '$ ' + input;
-  REPL_PRINT(output);
-};
-
-// Prints output from a command, and calls LOOP
-var REPL_PRINT = (output) => {
-  console.log(output);
-  REPL_LOOP();
-};
-
-// Clears and focuses input again
-var REPL_LOOP = () => {
-  document.getElementById('repl-text-input').value='';
-  focusOnInput();
-};
-
-
 /********************/
 /* React components */
 /********************/
 
-var REPLoutput = React.createClass({
+var REPL = React.createClass({
+  // Reads a command that was just submitted and passes it to EVALUATE
+  READ: function(event) {
+    event.preventDefault();
+    var input = document.getElementById('repl-text-input').value;
+    this.EVALUATE(input);
+  },
+  // Evaluate a command and passes it and its result to PRINT, then loops
+  EVALUATE: function(input) {
+    this.PRINT(this.state.whoami+' $ ' + input);
+    this.LOOP();
+  },
+  // Prints output from a command
+  PRINT: function(output) {
+    var newState = this.state;
+    newState.outputBuffer.push({key: newState.outputBuffer.length+1, text: output});
+    this.setState(newState);
+  },
+  // Clears and focuses input again
+  LOOP: function() {
+    document.getElementById('repl-text-input').value='';
+    focusOnInput();
+  },
   getInitialState: function() {
     return {
       outputBuffer: [
         {key: 0, text: 'Welcome to REPLogin'},
         {key: 1, text: 'Last login: Thu Sep 8 06:05:15 2016 from 46.120.5.205 (not really)'},
         {key: 2, text: '-bash: warning: This is not bash'}
-      ]
+      ],
+      whoami: '[tal@ater ~]'
     };
   },
+  componentDidMount: focusOnInput,
   render: function() {
     var outputLines = this.state.outputBuffer.map(
       (line) => ( <p key={line.key}>{line.text}</p> )
     );
     return (
-      <div className="repl-output">
-        {outputLines}
-      </div>
-    )
-  }
-});
-
-var REPLinput = React.createClass({
-  render: function() {
-    return (
-      <div className="repl-input">
-        <span className="whoami">[tal@ater ~] $</span>
-        <form onSubmit={REPL_READ}>
-          <input onBlur={focusOnInput} id="repl-text-input"></input>
-        </form>
-      </div>
-    )
-  }
-});
-
-var REPL = React.createClass({
-  componentDidMount: focusOnInput,
-  render: () => {
-    return (
       <div className="repl-container">
-        <REPLoutput />
-        <REPLinput />
+        <div className="repl-output">
+          {outputLines}
+        </div>
+        <div className="repl-input">
+          <span className="whoami">{this.state.whoami} $</span>
+          <form onSubmit={this.READ}>
+            <input onBlur={focusOnInput} id="repl-text-input"></input>
+          </form>
+        </div>
       </div>
     );
   }
